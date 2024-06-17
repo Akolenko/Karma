4c113d911f47d1f5949ddb025ab81725fc0eae35
dratumz <dratumzh187@gmail.com>
2024-06-17 12:29:02 +0300

    add axios

--- client/src/Components/BidForm/BidFrom.tsx
+++ client/src/Components/BidForm/BidFrom.tsx
@@ -8,25 +8,30 @@ interface Form {
 
 export default function BidForm() {
   const {register, handleSubmit} = useForm<Form>()
+  let successPostBid = null
   const submit: SubmitHandler<Form> = function (data) {
-    console.log(data)
+    console.log(data.title, data.description, data.address)
   }
 
-
   return (
-    <form onSubmit={handleSubmit(submit)} className={'flex flex-col bg-gray-200 p-6 w-80 rounded-xl gap-2'}>
-      <label>Название заявки</label>
-      <input placeholder={'Введите название'}
-             type={'text'} {...register('title', {required: true})} className={'p-2 rounded-xl'} />
-      <label className={'mt-2'}>Опишите Вашу проблему:</label>
-      <textarea spellCheck={"true"} rows={10} placeholder={'Текст проблемы'}
-                {...register('description', {required: true})} className={'p-2 rounded-xl'} />
-      <label className={'mt-2'}>Адрес</label>
-      <input placeholder={'Введите адрес по которому необходимо оказать помощь'}
-             type={'text'} {...register('address', {required: true})} className={'p-2 rounded-xl'} />
-      <button className={'mt-3 bg-blue-300'}>Создать заявку
-      </button>
-    </form>
+    <>
+      {
+        successPostBid ? <h2>Отправлено</h2> :
+          <form onSubmit={handleSubmit(submit)} className={'flex flex-col bg-gray-200 p-6 w-80 rounded-xl gap-2'}>
+            <label>Название заявки</label>
+            <input placeholder={'Введите название'}
+                   type={'text'} {...register('title', {required: true})} className={'p-2 rounded-xl'}/>
+            <label className={'mt-2'}>Опишите Вашу проблему:</label>
+            <textarea spellCheck={"true"} rows={10} placeholder={'Текст проблемы'}
+                      {...register('description', {required: true})} className={'p-2 rounded-xl'}/>
+            <label className={'mt-2'}>Адрес</label>
+            <input placeholder={'Введите адрес по которому необходимо оказать помощь'}
+                   type={'text'} {...register('address', {required: true})} className={'p-2 rounded-xl'}/>
+            <button className={'mt-3 bg-blue-300'}>Создать заявку
+            </button>
+          </form>
+      }
+    </>
   )
 
 }
