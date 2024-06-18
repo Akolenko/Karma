f5fc4aee169414e5c83f2a824c9c8d0fec72ff41
[Akolenko] <[andrejkolenko0@gmail.com]>
2024-06-18 22:19:33 +0300

    deploy

--- server/config/serverConfig.js
+++ server/config/serverConfig.js
@@ -2,14 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+}
 
 const serverConfig = (app) => {
   app.use(morgan("dev"));
   app.use(express.json());
   app.use(express.urlencoded({extended: true}));
-  app.use(cors({
-    credentials: 'true'
-  }))
+  app.use(cors(corsOptions))
 }
 
 module.exports = serverConfig;
