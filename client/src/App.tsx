fc9398431f2fd0e95bdb11c77540c367494251ee
dratumz <dratumzh187@gmail.com>
2024-06-18 13:26:52 +0300

    add Bid list component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import BidForm from "./Components/BidForm/BidFrom.tsx";
 import MainPage from "./Components/MainPage/MainPage.tsx";
 import { Route, Routes } from "react-router";
+import BidList from "./Components/BidList/BidList.tsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route index element={<MainPage/>}/>
         <Route path={'/bid-form'} element={<BidForm/>}/>
+        <Route path={'/bid-list'} element={<BidList/>}/>
       </Routes>
 
     </>
