46c12838f9287e02c03e4289c5b06b0b15f78f5e
[Akolenko] <[andrejkolenko0@gmail.com]>
2024-06-18 20:05:13 +0300

    merge3

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,18 +1,18 @@
-import Auth from '../pages/Auth';
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BidForm from "../Components/BidForm/BidFrom.tsx";
+import MainPage from "../Components/MainPage/MainPage.tsx";
+import { Route, Routes } from "react-router";
+import BidList from "../Components/BidList/BidList.tsx";
 
-function App(): JSX.Element {
-
+function App() {
   return (
     <>
-      <div>
-        <Router>
-          <Routes>
-            <Route path='/auth' element={< Auth />} />
-          </Routes>
-        </Router>
-      </div>
+      <Routes>
+        <Route index element={<MainPage/>}/>
+        <Route path={'/bid-form'} element={<BidForm/>}/>
+        <Route path={'/bid-list'} element={<BidList/>}/>
+      </Routes>
+
     </>
   )
 }
