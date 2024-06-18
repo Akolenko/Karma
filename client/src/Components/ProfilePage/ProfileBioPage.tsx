9f8f32c7c0342ddf29a538cb83ea4410088c0dd6
IrinaZot <zotovairinaa@yandex.ru>
2024-06-18 23:45:35 +0300

    profile 1.0

---
+++ client/src/Components/ProfilePage/ProfileBioPage.tsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import ProfilePage from "./ProfilePage";
+
+
+
+export type BioProfileType = {
+    id?: number,
+    fio:string,
+    date_of_birth:any, //???
+    email:string,
+    password:string,
+    phone:string
+}
+
+function ProfileBioPage(): JSX.Element {
+    const [user, setUser] = useState<BioProfileType[]>([]);
+  
+    useEffect(() => {
+      const user = axios("http://localhost:3000/profile")
+        .then((res) => setUser(res.data));
+    }, []);
+    // console.log(user);
+    
+  
+    return (
+        <>
+        <ProfilePage/>
+
+      <div className={"flex flex-col"}>
+        <img className={'object-cover h-60 w-96 my-20'} src="https://adindex.ru/files2/news/2019_07/273997_inkognito.jpg?ts=" alt="pic"/>
+        <div className={'font-extrabold text-xl col-start-1 col-end-3 flex-1'}>{user.fio}</div>
+        <div className={'font-extrabold'}>{user.email}</div>
+        <div className={'font-extrabold'}>{user.phone}</div>
+      </div>
+      
+        </>
+    );
+  }
+  
+  export default ProfileBioPage;
+  
--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -3,6 +3,9 @@ import BidForm from "../Components/BidForm/BidFrom.tsx";
 import MainPage from "../Components/MainPage/MainPage.tsx";
 import { Route, Routes } from "react-router";
 import BidList from "../Components/BidList/BidList.tsx";
+import ProfilePage from '../Components/ProfilePage/ProfilePage.tsx';
+import ProfileBioPage from '../Components/ProfilePage/ProfileBioPage.tsx';
+import ProfileBidPage from '../Components/ProfilePage/ProfileBidPage.tsx';
 
 function App() {
   return (
@@ -11,6 +14,10 @@ function App() {
         <Route index element={<MainPage/>}/>
         <Route path={'/bid-form'} element={<BidForm/>}/>
         <Route path={'/bid-list'} element={<BidList/>}/>
+        <Route path={"/profile"} element={<ProfilePage/>}/>
+       
+        <Route path={"/profile/bio"} element={<ProfileBioPage/>}/>
+        <Route path={"/profile/bid"} element={<ProfileBidPage/>}/>
       </Routes>
 
     </>
