773542f59b49495cab70da8c043f0ab8787d1af2
[Akolenko] <[andrejkolenko0@gmail.com]>
2024-06-19 20:44:26 +0300

    typescript

--- client/src/Components/ProfilePage/ProfileBioPage.tsx
+++ client/src/Components/ProfilePage/ProfileBioPage.tsx
@@ -2,12 +2,10 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import ProfilePage from "./ProfilePage";
 
-
-
 export type BioProfileType = {
     id?: number,
     fio:string,
-    date_of_birth:any, //???
+    date_of_birth: Date, //???
     email:string,
     password:string,
     phone:string
@@ -17,10 +15,10 @@ function ProfileBioPage(): JSX.Element {
     const [user, setUser] = useState<BioProfileType[]>([]);
   
     useEffect(() => {
-      const user = axios("http://localhost:3000/profile")
+        axios("http://localhost:3000/profile")
         .then((res) => setUser(res.data));
     }, []);
-    // console.log(user);
+    console.log('1', user);
     
   
     return (
@@ -29,9 +27,9 @@ function ProfileBioPage(): JSX.Element {
 
       <div className={"flex flex-col"}>
         <img className={'object-cover h-60 w-96 my-20'} src="https://adindex.ru/files2/news/2019_07/273997_inkognito.jpg?ts=" alt="pic"/>
-        <div className={'font-extrabold text-xl col-start-1 col-end-3 flex-1'}>{user.fio}</div>
-        <div className={'font-extrabold'}>{user.email}</div>
-        <div className={'font-extrabold'}>{user.phone}</div>
+        <div className={'font-extrabold text-xl col-start-1 col-end-3 flex-1'}>{user[0].fio}</div>
+        <div className={'font-extrabold'}>{user[0].email}</div>
+        <div className={'font-extrabold'}>{user[0].phone}</div>
       </div>
       
         </>
