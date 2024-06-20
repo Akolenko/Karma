389889018a5dbc86d2cd41219f1c453906904c0c
IrinaZot <zotovairinaa@yandex.ru>
2024-06-20 22:24:33 +0300

    profileVers2

--- client/src/Components/ProfilePage/ProfileBidPage.tsx
+++ client/src/Components/ProfilePage/ProfileBidPage.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-// import ProfilePage from "./ProfilePage";
+import ProfilePage from "./ProfilePage";
 import Bid from "../Bid/Bid"
-
+import { NavLink } from "react-router-dom";
 
 export type BidProfileType = {
     id: number,
@@ -20,20 +20,48 @@ function ProfileBidPage(): JSX.Element {
       axios("http://localhost:3000/profile/bid")
         .then((res) => setBids(res.data));
     }, []);
-    console.log(bids);
+    // console.log(bids);
     
   
     return (
         <>
-        {/*<ProfilePage/>*/}
+        <ProfilePage/>
 
-      <div className={"flex flex-col"}>
+        <NavLink to={"/profile/bid/active"}>
+          <button
+            className={
+              "w-80 border-lime-600 hover:bg-lime-500 hover:text-white hover:border-none bg-white text-lime-600"
+            }
+          >
+            Активные заявки
+          </button>
+        </NavLink>
+
+        <NavLink to={"/profile/bid/progress"}>
+          <button
+            className={
+              "w-80 border-lime-600 hover:bg-lime-500 hover:text-white hover:border-none bg-white text-lime-600"
+            }
+          >
+            Заявки в работе
+          </button>
+        </NavLink>
+        <NavLink to={"/profile/bid/closed"}>
+          <button
+            className={
+              "w-80 border-lime-600 hover:bg-lime-500 hover:text-white hover:border-none bg-white text-lime-600"
+            }
+          >
+            Закрытые заявки
+          </button>
+        </NavLink>
+
+      {/* <div className={"flex flex-col"}>
       {bids && bids.map((bid) => (<Bid key={bid.id} bid={bid} />))}
-      </div>
+      </div> */}
       
         </>
     );
   }
   
   export default ProfileBidPage;
-  
--- client/src/Components/ProfilePage/ProfilePage.tsx
+++ client/src/Components/ProfilePage/ProfilePage.tsx
@@ -1,65 +1,66 @@
-// import axios from "axios";
-// import { useState, useEffect, Children } from "react";
-// import { NavLink } from "react-router-dom";
-//
-// export type BioProfileType = {
-//   id?: number;
-//   fio: string;
-//   date_of_birth: any; //???
-//   email: string;
-//   password: string;
-//   phone: string;
-// };
-//
-// function ProfilePage(): JSX.Element {
-//   const [user, setUser] = useState<BioProfileType[]>([]);
-//
-//   useEffect(() => {
-//     const user = axios("http://localhost:3000/profile").then((res) =>
-//       setUser(res.data)
-//     );
-//   }, []);
-//   // console.log(user);
-//
-//   return (
-//     <div className="mainConteiner">
-//       <h2 className="profile-page-title">МОЯ СТРАНИЦА</h2>
-//
-//       <div className="profile-page-button">
-//         <NavLink to={"/profile/bio"}>
-//           <button
-//             className={
-//               "w-80 border-lime-600 hover:bg-lime-500 hover:text-white hover:border-none bg-white text-lime-600"
-//             }
-//           >
-//             Мои данные
-//           </button>
-//         </NavLink>
-//         <NavLink to={"/profile/bid"}>
-//           <button
-//             className={
-//               "w-80 border-lime-600 hover:bg-lime-500 hover:text-white hover:border-none bg-white text-lime-600"
-//             }
-//           >
-//             Мои заявки
-//           </button>
-//         </NavLink>
-//         <NavLink to={"/"}>
-//           <button
-//             className={
-//               "w-80 border-lime-600 hover:bg-lime-500 hover:text-white hover:border-none bg-white text-lime-600"
-//             }
-//           >
-//             Мои отклики
-//           </button>
-//         </NavLink>
-//       </div>
-//
-//       <div>
-//
-//       </div>
-//     </div>
-//   );
-// }
-//
-// export default ProfilePage;
+import axios from "axios";
+import { useState, useEffect, Children } from "react";
+import { NavLink } from "react-router-dom";
+
+export type BioProfileType = {
+  id?: number;
+  fio: string;
+  date_of_birth: any; //???
+  email: string;
+  password: string;
+  phone: string;
+};
+
+function ProfilePage(): JSX.Element {
+  const [user, setUser] = useState<BioProfileType[]>([]);
+
+  useEffect(() => {
+    const user = axios("http://localhost:3000/profile").then((res) =>
+      setUser(res.data)
+    );
+  }, []);
+  // console.log(user);
+
+  return (
+    <div className="mainConteiner">
+      <h2 className="profile-page-title">МОЯ СТРАНИЦА</h2>
+
+      <div className="profile-page-button">
+        <NavLink to={"/profile/bio"}>
+          <button
+            className={
+              "w-80 border-lime-600 hover:bg-lime-500 hover:text-white hover:border-none bg-white text-lime-600"
+            }
+          >
+            Мои данные
+          </button>
+        </NavLink>
+        <NavLink to={"/profile/bid"}>
+          <button
+            className={
+              "w-80 border-lime-600 hover:bg-lime-500 hover:text-white hover:border-none bg-white text-lime-600"
+            }
+          >
+            Мои заявки
+          </button>
+        </NavLink>
+        <NavLink to={"/"}>
+          <button
+            className={
+              "w-80 border-lime-600 hover:bg-lime-500 hover:text-white hover:border-none bg-white text-lime-600"
+            }
+          >
+            Мои отклики
+          </button>
+        </NavLink>
+      </div>
+
+      <div>
+
+      </div>
+    </div>
+  );
+}
+
+export default ProfilePage;
+
