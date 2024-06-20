c5bc23eb2913b96a565cc2c239958ce4607189ea
[Akolenko] <[andrejkolenko0@gmail.com]>
2024-06-20 20:19:58 +0300

    merge3

--- server/routes/API/bid.api.route.js
+++ server/routes/API/bid.api.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {Bid} = require('../../db/models/index');
+const {Bid} = require('../../db/models');
 
 router.post('/bids', async (req, res) => {
   const {title, description, address} = req.body;
   try {
+    console.log(req.body)
     const newBid = await Bid.create({
       title,
       description,
@@ -12,12 +13,14 @@ router.post('/bids', async (req, res) => {
       status: 'create',
       author_id: 2//TODO Хард-код, изменить при рабочей авторизации
     });
+    console.log('newBid', newBid);
     if (newBid) {
       res.status(201).json({title, description, address});
     } else {
       res.status(403).json({message: 'didnt create new Bid'})
     }
   } catch (e) {
+    console.log(e)
     res.status(400).json({message: "Didn't created bid!"});
   }
 
