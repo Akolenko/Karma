492054a44f78b2eb53b6103def29ab0122f4c189
[Akolenko] <[andrejkolenko0@gmail.com]>
2024-06-20 22:10:19 +0300

    merge auth

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,16 +1,19 @@
 import './App.css'
-import BidForm from "../Components/BidForm/BidForm.tsx";
-import MainPage from "../Components/MainPage/MainPage.tsx";
+import BidForm from "../components/BidForm/BidForm.tsx";
+import MainPage from "../components/MainPage/MainPage.tsx";
 import { Route, Routes } from "react-router";
 // import ProfilePage from '../Components/ProfilePage/ProfilePage.tsx';
 // import ProfileBioPage from '../Components/ProfilePage/ProfileBioPage.tsx';
-import ProfileBidPage from '../Components/ProfilePage/ProfileBidPage.tsx';
-import BidList from "../Components/BidList/BidList.tsx";
+import ProfileBidPage from '../components/ProfilePage/ProfileBidPage.tsx';
+import BidList from "../components/BidList/BidList.tsx";
+import LoginForm from "../components/LoginForm.tsx";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path={'/login'} element={<LoginForm/>}/>
+        {/*<Route path={'/register'} element={<RegisterForm/>}/>Форма регистрации*/}
         <Route index element={<MainPage/>}/>
         <Route path={'/bid-form'} element={<BidForm/>}/>
         <Route path={'/bid-list'} element={<BidList/>}/>
