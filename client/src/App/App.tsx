4c5f027084e494239e4ff1b1d3367c66241b93c0
[Akolenko] <[andrejkolenko0@gmail.com]>
2024-06-20 21:00:20 +0300

    kal

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,67 +1,26 @@
-
-import { useContext, useEffect, useState } from 'react'
 import './App.css'
-import axios from 'axios'
-import LoginForm from '../components/LoginForm'
-import { AuthContext } from '../main'
-import { observer } from 'mobx-react-lite'
-import { Bid } from '../models/response/Bid.interface'
-import BidsServise from '../services/BidsService'
+import BidForm from "../Components/BidForm/BidForm.tsx";
+import MainPage from "../Components/MainPage/MainPage.tsx";
+import { Route, Routes } from "react-router";
+// import ProfilePage from '../Components/ProfilePage/ProfilePage.tsx';
+// import ProfileBioPage from '../Components/ProfilePage/ProfileBioPage.tsx';
+import ProfileBidPage from '../Components/ProfilePage/ProfileBidPage.tsx';
+import BidList from "../Components/BidList/BidList.tsx";
 
 function App() {
-
-  const { authStore } = useContext(AuthContext)
-  const [bids, setBids] = useState<Bid[]>([])
-
-  useEffect(() => {
-    if (localStorage.getItem('token')) {
-      authStore.checkAuth()
-    }
-
-  }, [])
-
-  async function getBids() {
-    try {
-      const response = await BidsServise.fetchBids()
-      setBids(response.data)
-    } catch (error) {
-      console.log(error);
-      
-    }
-  }
-
-  if (authStore.isLoading) {
-    return <div>Загрузка...</div>
-  }
-  
-  if (!authStore.isAuth) {
-    return (<>
-    <LoginForm/>
-    <button onClick={getBids}>Посмотреть заявки</button>
-    </>)
-  }
-
-
   return (
     <>
-    <h1>{authStore.isAuth ? `Пользователь авторизован ${authStore.user.email}` : 'АВТОРИЗУЙТЕСЬ'}</h1>
-    <button onClick={() => authStore.logout()}>Выйти</button>
-    <div>
-      <button onClick={getBids}>Посмотреть заявки</button>
-      {
-        bids && bids.length ? bids.map(bid => {
-          return (
-            <div key={bid.id}>
-              <div>{bid.title}</div>
-              <div>{bid.description}</div>
-              <div>{bid.address}</div>
-            </div>
-          )
-        }) : <div>Заявок пока нет</div>
-      }
-    </div>
+      <Routes>
+        <Route index element={<MainPage/>}/>
+        <Route path={'/bid-form'} element={<BidForm/>}/>
+        <Route path={'/bid-list'} element={<BidList/>}/>
+        {/*<Route path={"/profile"} element={<ProfilePage/>}/>*/}
+        {/*<Route path={"/profile/bio"} element={<ProfileBioPage/>}/>*/}
+        <Route path={"/profile/bids"} element={<ProfileBidPage/>}/>
+      </Routes>
+
     </>
   )
 }
 
-export default observer(App)
+export default App
