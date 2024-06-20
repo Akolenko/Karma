3e27f12f1c5b20d39e0048dde6c4ed8fe578046e
IrinaZot <zotovairinaa@yandex.ru>
2024-06-20 22:24:33 +0300

    profileVers2

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -2,10 +2,13 @@ import './App.css'
 import BidForm from "../Components/BidForm/BidForm.tsx";
 import MainPage from "../Components/MainPage/MainPage.tsx";
 import { Route, Routes } from "react-router";
-// import ProfilePage from '../Components/ProfilePage/ProfilePage.tsx';
-// import ProfileBioPage from '../Components/ProfilePage/ProfileBioPage.tsx';
+import ProfilePage from '../Components/ProfilePage/ProfilePage.tsx';
+import ProfileBioPage from '../Components/ProfilePage/ProfileBioPage.tsx';
 import ProfileBidPage from '../Components/ProfilePage/ProfileBidPage.tsx';
 import BidList from "../Components/BidList/BidList.tsx";
+import ProfileActiveBidPage from '../Components/ProfilePage/ProfileActiveBidPage.tsx';
+import ProfileClosedBidPage from '../Components/ProfilePage/ProfileClosedBidPage.tsx';
+import ProfileProgressBidPage from '../Components/ProfilePage/ProfileProgressBidPage.tsx';
 
 function App() {
   return (
@@ -14,9 +17,12 @@ function App() {
         <Route index element={<MainPage/>}/>
         <Route path={'/bid-form'} element={<BidForm/>}/>
         <Route path={'/bid-list'} element={<BidList/>}/>
-        {/*<Route path={"/profile"} element={<ProfilePage/>}/>*/}
-        {/*<Route path={"/profile/bio"} element={<ProfileBioPage/>}/>*/}
+        <Route path={"/profile"} element={<ProfilePage/>}/>
+        <Route path={"/profile/bio"} element={<ProfileBioPage/>}/>
         <Route path={"/profile/bid"} element={<ProfileBidPage/>}/>
+        <Route path={"/profile/bid/active"} element={<ProfileActiveBidPage/>}/>
+        <Route path={"/profile/bid/progress"} element={<ProfileProgressBidPage/>}/>
+        <Route path={"/profile/bid/closed"} element={<ProfileClosedBidPage/>}/>
       </Routes>
 
     </>
