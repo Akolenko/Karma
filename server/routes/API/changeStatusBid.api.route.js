fd5b63f00738d0100981f1e9278da537c011f9bd
dratumz <dratumzh187@gmail.com>
2024-06-21 17:51:11 +0300

    edit name of status for bids

--- server/routes/API/changeStatusBid.api.route.js
+++ server/routes/API/changeStatusBid.api.route.js
@@ -7,8 +7,8 @@ router.put("/bids/:id", async (req, res) => {
 
   const bid = await Bid.findByPk(bidId);
   const currentBid = await Bid.findOne({where: {id: bid.id}});
-  const changeStatusBid = await currentBid.update({status: 'in progress'})
-  if (changeStatusBid.status === 'in progress') {
+  const changeStatusBid = await currentBid.update({status: 'response'})
+  if (changeStatusBid.status === 'response') {
     res.status(200).json({text: 'successful!'})
   } else {
     res.status(403).json({message: 'didnt update bid!'});
