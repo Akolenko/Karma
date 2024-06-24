8e98eb86ac5efdce7340a989985738af599689f0
Akolenko <andrejkolenko0@gmail.com>
2024-06-24 09:18:54 +0300

    navbar style

--- client/src/Components/Navbar/Navbar.tsx
+++ client/src/Components/Navbar/Navbar.tsx
@@ -6,58 +6,60 @@ const Navbar = (): JSX.Element => {
   const normalLink = 'text-[#515066] hover:text-[#51B85B] transition ease-in-out duration-100'
   
   return (
-    <header className='sticky inset-x-0 top-0 bg-white rounded-full mt-5 ml-2 mr-2 mb5 w-screen'>
-      <div className='mx-auto flex items-center justify-between p-4" aria-label="Global'>
-        <div>
+    <header className='sticky inset-x-0 bg-green-600 top-0 pt-5 pl-2 pr-2 pb-5'>
+      <div className="bg-white rounded-full">
+        <div className='mx-auto flex items-center justify-between p-4" aria-label="Global'>
+          <div>
 
-          <NavLink
-            className='flex lg:flex-1 -m-1.5 p-1.5'
-            to='/'>
-            <img className='h-8 w-auto' src="/public/svg/Vector.svg" alt="logo"/>
-          </NavLink>
-
-        </div>
-        <div className='items-center'>
-
-          <button type='button'>
             <NavLink
-              className={({ isActive }) =>
-                isActive ? activeLink : normalLink}
+              className='flex lg:flex-1 -m-1.5 p-1.5'
               to='/'>
-              Главная
+              <img className='h-8 w-auto' src="/public/svg/Vector.svg" alt="logo"/>
             </NavLink>
-          </button>
 
-          <button type='button'>
-            <NavLink
-              className={({ isActive }) =>
-                isActive ? activeLink : normalLink}
-              to='/bid-list'>
-              Список заявок
-            </NavLink>
-          </button>
+          </div>
+          <div className='items-center'>
 
-          <button type='button'>
-            <NavLink
-              className={({ isActive }) =>
-                isActive ? activeLink : normalLink}
-              to='/profile'>
-              Моя страница
-            </NavLink>
-          </button>
+            <button type='button'>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? activeLink : normalLink}
+                to='/'>
+                Главная
+              </NavLink>
+            </button>
 
-        </div>
-        <div>
+            <button type='button'>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? activeLink : normalLink}
+                to='/bid-list'>
+                Список заявок
+              </NavLink>
+            </button>
 
-          <button type='button'>
-            <NavLink
-              className={({ isActive }) =>
-                isActive ? activeLink : normalLink}
-              to='/login'>
-              Войти
-            </NavLink>
-          </button>
+            <button type='button'>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? activeLink : normalLink}
+                to='/profile'>
+                Моя страница
+              </NavLink>
+            </button>
 
+          </div>
+          <div>
+
+            <button type='button'>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? activeLink : normalLink}
+                to='/login'>
+                Войти
+              </NavLink>
+            </button>
+
+          </div>
         </div>
       </div>
     </header>
