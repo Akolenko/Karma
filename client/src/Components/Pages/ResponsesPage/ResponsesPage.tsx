accaf4fbbcb4f3bccafcd16c2b8cfd0e4131b9ee
dratumz <dratumzh187@gmail.com>
2024-06-25 13:21:11 +0300

    render responses

---
+++ client/src/Components/Pages/ResponsesPage/ResponsesPage.tsx
@@ -0,0 +1,30 @@
+import { useAppDispatch, useAppSelector } from "../../../../hooks/redux.ts";
+import { useEffect } from "react";
+import ProfilePage from "../../ProfilePage/ProfilePage.tsx";
+import BidResponse from "../../Bid/BidResponse.tsx";
+import { getResponses } from "../../../../features/userResponseSlice.ts";
+
+export default function ResponsesPage() {
+  const dispatch = useAppDispatch();
+  const responses = useAppSelector(state => state.responseBid.bids)
+
+  useEffect(() => {
+    dispatch(getResponses())
+  }, [dispatch])
+
+
+  return (
+    <>
+      <ProfilePage/>
+      <div className={'flex flex-col gap-2 mt-10'}>
+        {responses && responses.length ?
+          responses.map((response) => (<BidResponse key={response.id} response={response} />))
+          :
+          <div>Вы еще не откликнулись на заявки!</div>
+        }
+      </div>
+    </>
+
+  )
+
+}
