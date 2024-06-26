0d6f441c0ee416901cbd527eff782005b093b236
IrinaZot <zotovairinaa@yandex.ru>
2024-06-26 20:13:40 +0300

    profilevers4

--- client/src/redux/reducers/root.reducer.ts
+++ client/src/redux/reducers/root.reducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import counterReducer from  "../../features/counterSlice"
 import postsReducer from "../../features/postsSlice"
+import  useReducer from "../../../features/userEditProfileSlice";
 
 export const rootReducer = combineReducers({
     counter:  counterReducer,
-    posts: postsReducer
+    posts: postsReducer,
+    user: useReducer,
 })
