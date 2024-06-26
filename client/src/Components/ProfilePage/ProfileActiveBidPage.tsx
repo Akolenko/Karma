b4c35a8577a6a149cc1737ea43eaefe239e6e05f
dratumz <dratumzh187@gmail.com>
2024-06-26 12:40:03 +0300

    add edit modal form

--- client/src/Components/ProfilePage/ProfileActiveBidPage.tsx
+++ client/src/Components/ProfilePage/ProfileActiveBidPage.tsx
@@ -1,29 +1,16 @@
-import {  useEffect } from "react";
+import { useEffect } from "react";
 import ProfileBidPage from "./ProfileBidPage";
 import ActiveBid from "../Bid/ActiveBid.tsx";
 import { useAppDispatch, useAppSelector } from "../../../hooks/redux.ts";
 import { getUserBids } from "../../../features/bidsUserSlice.ts";
 
-export type BidProfileType = {
-  id: number,
-  title: string,
-  description: string,
-  address: string,
-  status: string,
-  author_id: number
-}
-
 function ProfileActiveBidPage(): JSX.Element {
-  // const [bids, setBids] = useState<BidProfileType[]>([]);
   const bids = useAppSelector(state => state.userBids.list);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(getUserBids())
   }, [dispatch]);
-
-  //TODO: так как это страница отвечает за отображение заявок созданных пользователем,
-  // TODO: то в дальнейшем надо выполнять проверку user_id и author_id для скрытия кнопки откликнуться на "закрыть заявку"
 
   return (
     <>
