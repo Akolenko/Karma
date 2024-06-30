3767c4b0e1192a70f6ce438b43c26fc7bf679ba2
dratumz <dratumzh187@gmail.com>
2024-06-30 10:03:33 +0300

    add some styles for navbar & Main page

--- client/src/Components/MainPage/MainPage.tsx
+++ client/src/Components/MainPage/MainPage.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import './MainPage.styles.css'
 
 export default function MainPage() {
 
@@ -6,54 +7,63 @@ export default function MainPage() {
 
   return (
     <>
-    <div>
-      {
-        token ? <div className={'flex flex-wrap h-44 gap-5'}>
-        <h1 className={'w-7/12 font-medium'}>Найдите исполнителя или <br/> станьте исполнителем сами</h1>
-        <div className={''}>
-          <img src={''} alt={'Здесь будет какая-нибудь картинка красивая'}/>
-        </div>
-        <div className={'flex mt-5'}>
-          <Link to={'/bid-form'}>
-            <button
-              className={'focus:outline-none transition duration-300 w-80 mr-14 border-lime-600 hover:bg-lime-600' +
-                ' hover:text-white bg-white text-lime-600 '}>
-              Нужна помощь
-            </button>
-          </Link>
-          <Link to={'/bids-list'}>
-            <button
-              className={'focus:outline-none transition duration-300 w-80 border-lime-600 hover:bg-lime-600' +
-                ' hover:text-white bg-white text-lime-600'}>
-              Хочу помочь
-            </button>
-          </Link>
-        </div>
-      </div> : 
-      <div className={'flex flex-wrap h-44 gap-5'}>
-      <h1 className={'w-7/12 font-medium'}>Найдите исполнителя или <br/> станьте исполнителем сами</h1>
-      <div className={''}>
-        <img src={''} alt={'Здесь будет какая-нибудь картинка красивая'}/>
+      <div className={'start-el'}>
+        {
+          token ?
+            <div className={' flex flex-wrap h-44 gap-5 px-10 mt-[150px] ml-[200px]'}>
+              <h1 className={'w-7/12 font-medium'}>
+                KARMA <br/>
+                Найдите исполнителя или <br/>
+                станьте исполнителем сами</h1>
+              <div className={'w-1/4'}>
+                <img src={'./img/main_img.png'} alt={'Здесь будет какая-нибудь картинка красивая'}/>
+              </div>
+              <div className={'flex -mt-10'}>
+                <Link to={'/bid-form'}>
+                  <button
+                    className={'focus:outline-none transition' +
+                      ' duration-300 w-80 mr-14 border-lime-600 hover:bg-lime-600' +
+                      ' hover:text-white bg-white text-lime-600 shadow-md'}>
+                    Нужна помощь
+                  </button>
+                </Link>
+                <Link to={'/bids-list'}>
+                  <button
+                    className={'focus:outline-none transition duration-300 ' +
+                      'w-80 border-lime-600 hover:bg-lime-600' +
+                      ' hover:text-white bg-white text-lime-600 shadow-md'}>
+                    Хочу помочь
+                  </button>
+                </Link>
+              </div>
+            </div> :
+            <div className={'flex flex-wrap h-44 gap-5'}>
+              <h1 className={'w-7/12 font-medium'}>
+                KARMA <br/>
+                Найдите исполнителя или <br/>
+                станьте исполнителем сами</h1>
+              <div className={''}>
+                <img src={''} alt={'Здесь будет какая-нибудь картинка красивая'}/>
+              </div>
+              <div className={'flex mt-5'}>
+                <Link to={'/login'}>
+                  <button
+                    className={'focus:outline-none transition duration-300 w-80 mr-14 border-lime-600 hover:bg-lime-600' +
+                      ' hover:text-white bg-white text-lime-600 '}>
+                    Нужна помощь
+                  </button>
+                </Link>
+                <Link to={'/login'}>
+                  <button
+                    className={'focus:outline-none transition duration-300 w-80 border-lime-600 hover:bg-lime-600' +
+                      ' hover:text-white bg-white text-lime-600'}>
+                    Хочу помочь
+                  </button>
+                </Link>
+              </div>
+            </div>
+        }
       </div>
-      <div className={'flex mt-5'}>
-        <Link to={'/login'}>
-          <button
-            className={'focus:outline-none transition duration-300 w-80 mr-14 border-lime-600 hover:bg-lime-600' +
-              ' hover:text-white bg-white text-lime-600 '}>
-            Нужна помощь
-          </button>
-        </Link>
-        <Link to={'/login'}>
-          <button
-            className={'focus:outline-none transition duration-300 w-80 border-lime-600 hover:bg-lime-600' +
-              ' hover:text-white bg-white text-lime-600'}>
-            Хочу помочь
-          </button>
-        </Link>
-      </div>
-    </div>
-      }
-    </div>
 
     </>
   )
