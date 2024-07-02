5a6f06b85257a2ad5556001e7b4a7d4dddf45ec5
dratumz <dratumzh187@gmail.com>
2024-07-03 01:51:49 +0300

    edit complete bids and progress bids pages

--- client/src/Components/Bid/ActiveBid.tsx
+++ client/src/Components/Bid/ActiveBid.tsx
@@ -3,32 +3,46 @@ import { Bid, deleteUserBid } from "../../../features/bidsUserSlice.ts";
 import EditBidModal from "../UI/Modal/EditBidModal/EditBidModal.tsx";
 import { useState } from "react";
 
-export default function ActiveBid({bid}: { bid: Bid }) {
+export default function ActiveBid({ bid }: { bid: Bid }) {
   const [isEditModalOpen, setEditModalOpen] = useState(false);
   const dispatch = useAppDispatch();
-  const userId = localStorage.getItem('userId'); // TODO: можно попробовать вынести в отдельный файл.
+  const userId = localStorage.getItem("userId"); // TODO: можно попробовать вынести в отдельный файл.
 
   const deleteHandler = () => {
-    dispatch(deleteUserBid({bidId: bid.id, userId}))
-  }
+    dispatch(deleteUserBid({ bidId: bid.id, userId }));
+  };
 
   return (
     <>
-      <div className={'rounded-md bg-white p-3 text-left hover:scale-105 transition duration-300 pl-8'}>
-        <h3 className={'text-lg font-semibold tracking-wide leading-8'}>{bid.title}</h3>
-        <div className={'flex gap-x-2'}>
-          <img className={'w-4'} src={'/svg/Vector.svg'} alt={bid.title}/>
-          <p className={'text-sm font-sans tracking-wide leading-8 text-gray-500'}>{bid.address}</p>
+      <div
+        className={
+          "start-bid rounded-md bg-white p-3 text-left hover:scale-[1.02] transition duration-300 pl-8 shadow-md "
+        }
+      >
+        <h3 className={"text-lg font-semibold tracking-wide leading-8"}>
+          {bid.title}
+        </h3>
+        <div className={"flex gap-x-2"}>
+          <img className={"w-4"} src={"/svg/Vector.svg"} alt={bid.title} />
+          <p
+            className={
+              "text-sm font-sans tracking-wide leading-8 text-gray-500"
+            }
+          >
+            {bid.address}
+          </p>
         </div>
-        <div className={'flex justify-between items-baseline -mt-2'}>
+        <div className={"flex justify-between items-baseline"}>
           <button onClick={() => setEditModalOpen(true)}>Редактировать</button>
           <button onClick={deleteHandler}>Удаление</button>
         </div>
       </div>
 
-      <EditBidModal isOpen={isEditModalOpen}
-                    onRequestClose={() => setEditModalOpen(false)}
-                    bid={bid}/>
+      <EditBidModal
+        isOpen={isEditModalOpen}
+        onRequestClose={() => setEditModalOpen(false)}
+        bid={bid}
+      />
     </>
-  )
+  );
 }
--- client/src/Components/Bid/BidInProgress.tsx
+++ client/src/Components/Bid/BidInProgress.tsx
@@ -1,39 +1,52 @@
 import { Bid, completeUserBids } from "../../../features/bidsUserSlice.ts";
 import { useAppDispatch } from "../../../hooks/redux.ts";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 // export type bidId = object
 
-export default function BidProgress({bid}: { bid: Bid }) {
+export default function BidProgress({ bid }: { bid: Bid }) {
   const dispatch = useAppDispatch();
-  const userId = localStorage.getItem('userId');
+  const userId = localStorage.getItem("userId");
   const completeHandler = () => {
-    dispatch(completeUserBids({bidId: bid.id, userId}));
-  }
+    dispatch(completeUserBids({ bidId: bid.id, userId }));
+  };
 
   return (
     <>
-      <div className={'rounded-md bg-white p-3 text-left hover:scale-105 transition duration-300 pl-8'}>
-        <h3 className={'text-lg font-semibold tracking-wide leading-8'}>{bid.title}</h3>
-        <div className={'flex gap-x-2'}>
-          <img className={'w-4'} src={'/svg/Vector.svg'} alt={bid.title}/>
-          <p className={'text-sm font-sans tracking-wide leading-8 text-gray-500'}>{bid.address}</p>
+      <div
+        className={
+          "start-bid rounded-md bg-white p-3 text-left hover:scale-[1.02] transition duration-300 pl-8 shadow-md p-5"
+        }
+      >
+        <h3 className={"text-lg font-semibold tracking-wide leading-8"}>
+          {bid.title}
+        </h3>
+        <div className={"flex gap-x-2"}>
+          <img className={"w-4"} src={"/svg/Vector.svg"} alt={bid.title} />
+          <p
+            className={
+              "text-sm font-sans tracking-wide leading-8 text-gray-500"
+            }
+          >
+            {bid.address}
+          </p>
         </div>
-        <div className={'flex flex-row justify-between items-baseline -mt-2'}>
-          <p className={'font-serif'}>{'Исполнитель: Имя исполнителя'}</p>
+        <div className={"flex flex-row justify-between items-baseline -mt-2"}>
+          <p className={"font-serif"}>{"Исполнитель: Имя исполнителя"}</p>
           <div>
-            <div className={'flex justify-between'}>
-              <NavLink
-                to={`/chat/?chat=${bid.id}&choise=${true}`}
-              >
-                <img className={'w-10'} src={'/svg/MailOutlined.svg'} alt='logo'/>
+            <div className={"flex justify-between items-center"}>
+              <button onClick={completeHandler}>Помощь оказана</button>
+              <NavLink to={`/chat/?chat=${bid.id}&choise=${true}`}>
+                <img
+                  className={"w-10"}
+                  src={"/svg/MailOutlined.svg"}
+                  alt='logo'
+                />
               </NavLink>
-              <img className={'w-10'} src={'/svg/PhoneOutlined.svg'} alt='logo'/>
             </div>
-            <button onClick={completeHandler}>Помощь оказана</button>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
