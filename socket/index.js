1335e76dfdc5ec8a13a084586befc0612c7a6d94
[Akolenko] <[andrejkolenko0@gmail.com]>
2024-07-02 23:47:41 +0300

    1

--- socket/index.js
+++ socket/index.js
@@ -51,3 +51,9 @@ io.on('connection', (socket) => {
 server.listen(port, () => {
   console.log('Listening on port ' + port);
 });
+
+// location /socket.io/ {
+//   proxy_pass http://localhost:4000;
+// proxy_http_version 1.1;
+// proxy_set_header Upgrade $http_upgrade;
+// proxy_set_header Connection "upgrade";
