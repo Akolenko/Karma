e9253b0a49da02404b1b2c503879e1e43e3f87a3
dratumz <dratumzh187@gmail.com>
2024-07-02 22:31:59 +0300

    add font-style-bid-form

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,23 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  mode: 'jit',
-  content: [
-    "./index.html",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+  mode: "jit",
+  content: ["./index.html", "./src/components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
-const withMT = require("@material-tailwind/react/utils/withMT");
-
-module.exports = withMT({
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-});
-
+};
