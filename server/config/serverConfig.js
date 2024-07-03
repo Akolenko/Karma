42ee5212e1936ac160cf0b73759cf2123a62e287
[Akolenko] <[andrejkolenko0@gmail.com]>
2024-07-04 01:13:00 +0300

    cors debug

--- server/config/serverConfig.js
+++ server/config/serverConfig.js
@@ -5,7 +5,7 @@ const errorMiddleware = require('../middleware/error-middleware');
 const cookieParser = require('cookie-parser');
 
 const corsOptions = {
-  origin: 'http://46.148.228.8',
+  origin: 'http://46.148.228.8:80',
   optionsSuccessStatus: 200,
   credentials: true,
 };
