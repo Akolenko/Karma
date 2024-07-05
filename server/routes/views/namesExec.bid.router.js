293314fea50a7331a9c763e0c8f51026c091e878
dratumz <dratumzh187@gmail.com>
2024-07-05 23:33:58 +0300

    delete get names exec

--- server/routes/views/namesExec.bid.router.js
+++ server/routes/views/namesExec.bid.router.js
@@ -4,11 +4,11 @@ const { User, Response } = require("../../db/models");
 
 router.get("/names-exec", async (req, res) => {
   const { bidId } = req.query;
-  console.log(bidId);
   try {
     const execId = await Response.findOne({ where: { bid_id: bidId } });
+    console.log(execId);
     const currExec = await User.findByPk(execId.user_id);
-    console.log(currExec);
+    console.log(currExec.fio);
     res.status(201).json(currExec.fio);
   } catch (e) {
     res.send(e.message);
